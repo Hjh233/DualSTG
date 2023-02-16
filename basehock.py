1fcf15e0a1bb2da563d16b41b69ebd9a49c210d9
huangjiahui <825817999@qq.com>
2023-02-16 19:51:22 +0800

    feat:compare inference time consumption bewteen stg and dualstg

--- basehock.py
+++ basehock.py
@@ -42,13 +42,106 @@ basehock_zeta = np.array([0.1, 0.3, 0.5, 1.0, 2.0, 3.0, 4.0, 5.0, 10.0])
 btm_z_overlap = []
 
 if __name__ == "__main__":
-    # parser = argparse.ArgumentParser()
-    # parser.add_argument('--zeta', type=float, required=True)
+    # STG Training Stage
+    # mus = None
 
-    # args = parser.parse_args()
+    # models, top_model = VFL.make_binary_models(
+    #     input_dim_list=input_dim_list,
+    #     type="STG",
+    #     emb_dim=8,
+    #     output_dim=output_dim,
+    #     hidden_dims=[16, 8],
+    #     activation="relu",
+    #     mus=mus, lam=0.1,
+    #     zeta=0)
 
-    # zeta = args.zeta
+    # original_gate_history, _ = VFL.train(
+    #     models,
+    #     top_model,
+    #     train_loader,
+    #     val_loader,
+    #     test_loader,
+    #     epochs=80,
+    #     optimizer='Adam',
+    #     criterion=criterion,
+    #     verbose=True,
+    #     models_save_dir='Checkpoints/basehock_stg_models.pt',
+    #     top_model_save_dir='Checkpoints/basehock_stg_top_model.pt',
+    #     save_mask_at=100000, 
+    #     freeze_top_till=0)
 
+    # # print(original_gate_history)
+    # print(original_gate_history.tail())
+
+    # original_gate_history.to_csv('LDPLog/basehock_original_gate.csv')
+
+    # mus = None
+
+    # models, top_model = VFL.make_binary_models(
+    #     input_dim_list=input_dim_list,
+    #     type="STG",
+    #     emb_dim=8,
+    #     output_dim=output_dim,
+    #     hidden_dims=[16, 8],
+    #     activation="relu",
+    #     mus=mus, lam=0.1,
+    #     zeta=0)
+    
+    # models_path = 'Checkpoints/basehock_stg_models.pt'
+    # top_model_path = 'Checkpoints/basehock_stg_top_model.pt'
+
+    # models_checkpoint = torch.load(models_path)
+
+    # models[0].load_state_dict(models_checkpoint['model_0_state_dict'])
+    # models[1].load_state_dict(models_checkpoint['model_1_state_dict'])
+    # models[2].load_state_dict(models_checkpoint['model_2_state_dict'])
+
+    # # models.load_state_dict(torch.load(models_path))
+    # top_model.load_state_dict(torch.load(top_model_path))
+
+    # VFL.inference(models, top_model, test_loader)
+
+
+
+
+    # DualSTG Training Phase
+
+    # gini_labels = dataset.gini_filter(0.5)
+    # feat_idx_list = dataset.get_feature_index_list()
+
+    # mus = VFL.initialize_mu(gini_labels, feat_idx_list)
+    # models, top_model = VFL.make_binary_models(
+    #     input_dim_list=input_dim_list,
+    #     type="DualSTG",
+    #     emb_dim=8,
+    #     output_dim=output_dim,
+    #     hidden_dims=[16, 8],
+    #     activation="relu",
+    #     mus=mus, top_lam=0.1, lam=0.1,
+    #     zeta=0)
+
+    # dual_stg_gini_history, _ = VFL.train(
+    #     models,
+    #     top_model,
+    #     train_loader,
+    #     val_loader,
+    #     test_loader,
+    #     epochs=80,
+    #     optimizer='Adam',
+    #     criterion=criterion,
+    #     verbose=True,
+    #     models_save_dir='Checkpoints/basehock_dualstg_models.pt',
+    #     top_model_save_dir='Checkpoints/basehock_dualstg_top_model.pt',
+    #     save_mask_at=100000, 
+    #     freeze_top_till=0)
+
+    # print(dual_stg_gini_history.tail())
+
+    # # dual_stg_gini_history.to_csv('LDPLog/basehock_ldp_{}.csv'.format(zeta))
+
+   
+    # # DualSTG Inference Phase
+    
     gini_labels = dataset.gini_filter(0.5)
     feat_idx_list = dataset.get_feature_index_list()
 
@@ -62,56 +155,21 @@ if __name__ == "__main__":
         activation="relu",
         mus=mus, top_lam=0.1, lam=0.1,
         zeta=0)
+    
+    models_path = 'Checkpoints/basehock_dualstg_models.pt'
+    top_model_path = 'Checkpoints/basehock_dualstg_top_model.pt'
 
-    dual_stg_gini_history, bottom_z_list_baseline = VFL.train(
-        models,
-        top_model,
-        train_loader,
-        val_loader,
-        test_loader,
-        epochs=80,
-        optimizer='Adam',
-        criterion=criterion,
-        verbose=True,
-        save_mask_at=100000, 
-        freeze_top_till=0)
 
-    # print(dual_stg_gini_history.tail())
+    models_checkpoint = torch.load(models_path)
 
-    # dual_stg_gini_history.to_csv('LDPLog/basehock_ldp_{}.csv'.format(zeta))
-    for i in range(len(basehock_zeta)):
-        models, top_model = VFL.make_binary_models(
-            input_dim_list=input_dim_list,
-            type="DualSTG",
-            emb_dim=8,
-            output_dim=output_dim,
-            hidden_dims=[16, 8],
-            activation="relu",
-            mus=mus, top_lam=0.8, lam=0.2,
-            zeta=basehock_zeta[i])
+    models[0].load_state_dict(models_checkpoint['model_0_state_dict'])
+    models[1].load_state_dict(models_checkpoint['model_1_state_dict'])
+    models[2].load_state_dict(models_checkpoint['model_2_state_dict'])
 
-        dual_stg_gini_history, bottom_z_list = VFL.train(
-            models,
-            top_model,
-            train_loader,
-            val_loader,
-            test_loader,
-            epochs=80,
-            optimizer='Adam',
-            criterion=criterion,
-            verbose=True,
-            save_mask_at=100000, 
-            freeze_top_till=0)
-        
-        client_0_overlap = np.sum(np.int64(bottom_z_list_baseline[0]>0) * np.int64(bottom_z_list[0]>0)) / np.count_nonzero(np.int64(bottom_z_list_baseline[0]>0))
-        client_1_overlap = np.sum(np.int64(bottom_z_list_baseline[1]>0) * np.int64(bottom_z_list[1]>0)) / np.count_nonzero(np.int64(bottom_z_list_baseline[1]>0))
-        server_overlap = np.sum(np.int64(bottom_z_list_baseline[2]>0) * np.int64(bottom_z_list[2]>0)) / np.count_nonzero(np.int64(bottom_z_list_baseline[2]>0))
+    # models.load_state_dict(torch.load(models_path))
+    top_model.load_state_dict(torch.load(top_model_path))
 
-        overlap_ratio = (client_0_overlap + client_1_overlap + server_overlap) / 3
+    VFL.inference(models, top_model, test_loader)
 
-        btm_z_overlap.append(overlap_ratio)
 
-    print(btm_z_overlap)
-    round_overlap = [round(x, 3) for x in btm_z_overlap]
-    print(round_overlap)
-
+    
